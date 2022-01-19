6b5d463dce7a46a21c897e54d3024424dafae8bd
Jesse Hernandez <jessehernandez@MacBook-Pro.local>
2022-01-18 20:35:43 -0800

    established mongoose connection and created Items schema

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-
 
 //configure ejs
 app.set('view engine', 'ejs');
@@ -14,7 +14,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 //ask ejs to serve the public folder with static files
 app.use(express.static("public"));
 
-var items = ["Buy butter", "Gym", "Code"];
+//mongoose server connection
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+//todo list item schema
+const itemsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'No item added'}
+});
+
+//mongoose model initializer: creates collection
+const Item = mongoose.model("Item", itemsSchema);
+
+//create document to add to collection
+const item1 = new Item ({
+        name: 'Welcome to your todolist'
+});
+
+//delete documents in collection
+// Item.deleteMany({_id: ""}, function(err){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Succesfully removed items from the todo list");
+//     }
+// })
+
+
+// item.save();
+
+
+// var items = ["Buy butter", "Gym", "Code"];
 var workItems = [];
 
 //home page
